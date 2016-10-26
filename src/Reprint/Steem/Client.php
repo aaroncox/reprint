d85413c65e7d6a14c4c98d722e826d39e04c6956
Aaron Cox <acox@carbinestudios.com>
2016-10-26 00:47:35 -0700

    Implemented greymass/steemphp instead of using our own API library

--- src/Reprint/Steem/Client.php
+++ src/Reprint/Steem/Client.php
@@ -2,10 +2,9 @@
 
 namespace Reprint\Steem;
 
-use JsonRPC\Client as RpcClient;
-use JsonRPC\HttpClient;
+use Greymass\SteemPHP\RPC;
+use Greymass\SteemPHP\Data\Comment;
 
-use Reprint\Model\Comment;
 
 class Client
 {
@@ -17,9 +16,7 @@ class Client
   {
     $this->config = $config;
     $this->db = $db;
-    $httpClient = new HttpClient($this->getConfig('host'));
-    $httpClient->withoutSslVerification();
-    $this->client = new RpcClient($this->getConfig('host'), false, $httpClient);
+    $this->client = new RPC($this->getConfig('host'));
   }
 
   public function getConfig($key) {
@@ -29,32 +26,26 @@ class Client
     return null;
   }
 
-  public function getPost($username, $permlink) {
-    $client = $this->client;
-    $response = $client->get_content($username, $permlink);
-    $post = new Comment($response);
-    return $post;
-  }
-
-  protected function amendPosts($posts, $tags = []) {
-    $return = [];
-    foreach($posts as $data) {
-      $post = new Comment($data);
-      if(empty($tags) || in_array($post->category, $tags) || ($post->json_metadata && count(array_intersect($tags, $post->json_metadata['tags'])) > 0)) {
-        $return[] = $post;
+  public function getContent() {
+    $accounts = $this->getConfig('accounts');
+    $tags = $this->getConfig('tags');
+    $content = array();
+    foreach($accounts as $name => $data) {
+      if(in_array('post', $data)) {
+        $content = array_merge($content, $this->getPosts($name, $tags));
       }
+      // if(in_array('reblog', $data)) {
+      //   $content = array_merge($content, $this->getReblogs($name, $tags));
+      // }
+      // if(in_array('vote', $data)) {
+
+      // }
     }
-    return $return;
+    $content = $this->sortContent($content);
+    return $content;
   }
 
-  public function getPostsFromAccount($account, $tags, $limit, $skip)
-  {
-    $client = $this->client;
-    $response = $client->get_state('@' . $account);
-    return $this->amendPosts($response['content'], $tags);
-  }
-
-  public function sortPosts($posts) {
+  public function sortContent($posts) {
     // Sort the posts by the new timestamp
     uasort($posts, function($a, $b) {
       if ($a->ts == $b->ts) {
@@ -63,41 +54,6 @@ class Client
       return ($a->ts < $b->ts) ? 1 : -1;
     });
     return $posts;
-  }
-
-  public function getContent() {
-    $accounts = $this->getConfig('accounts');
-    $tags = $this->getConfig('tags');
-    $posts = array();
-    foreach($accounts as $name => $data) {
-      if(in_array('post', $data)) {
-        $posts = array_merge($posts, $this->getPosts($name, $tags));
-      }
-      if(in_array('reblog', $data)) {
-        $posts = array_merge($posts, $this->getReblogs($name, $tags));
-      }
-      if(in_array('vote', $data)) {
-
-      }
-    }
-    return $posts;
-  }
-
-  public function getReblogs($account, $tags)
-  {
-    // Temporary solution using steemdb until we get proper APIs
-    $url = sprintf('https://steemdb.com/api/account/%s/contentreblog', $account);
-    $json = json_decode(file_get_contents($url), true);
-    $response = array();
-    // Local Storage
-    // $db = new Database('../var/cache/database/reblog');
-    foreach($json as $reblog) {
-      // Add the content to our response
-      $response[] = $content = new Comment($reblog['content'][0]);
-      // Check to see if it exists in the local database
-      // $this->cache($content['_id'], $content);
-    }
-    return $response;
   }
 
   private function cache($id, $content) {
@@ -121,24 +77,41 @@ class Client
   public function getPosts($account, $tags = array(), $limit = 5, $skip = 0)
   {
     $posts = $this->getPostsFromAccount($account, $tags, $limit, $skip);
-    // Sort these posts by timestamp
-    $posts = $this->sortPosts($posts);
+    // Sort the posts chronologically
+    $posts = $this->sortContent($posts);
     // Slice to get our desired amount
     $posts = array_slice($posts, $skip, $limit);
     // Return our posts
     return $posts;
   }
 
-  public function getApi($name)
+  public function getPostsFromAccount($account, $tags, $limit, $skip)
   {
-    return $this->client->call(1, 'get_api_by_name', [$name]);
+    $response = $this->client->get_state('@' . $account);
+    $return = [];
+    foreach($response->content as $data) {
+      if(empty($tags) || in_array($data->category, $tags) || ($data->json_metadata && count(array_intersect($tags, $data->json_metadata['tags'])) > 0)) {
+        $return[] = $data;
+      }
+    }
+    return $return;
   }
 
-  public function getFollowing($username, $limit = 100, $skip = -1)
+  public function getReblogs($account, $tags)
   {
-    // Load the appropriate API
-    $api = $this->getApi('follow_api');
-    // Get our followers
-    return $this->client->call($api, 'get_following', [$username, $skip, $limit]);;
+    // Temporary solution using steemdb until we get proper APIs
+    $url = sprintf('https://steemdb.com/api/account/%s/contentreblog', $account);
+    $json = json_decode(file_get_contents($url), true);
+    $response = array();
+    // Local Storage
+    // $db = new Database('../var/cache/database/reblog');
+    foreach($json as $reblog) {
+      // Add the content to our response
+      // $response[] = $content = new Comment($reblog['content'][0]);
+      // Check to see if it exists in the local database
+      // $this->cache($content['_id'], $content);
+    }
+    return $response;
   }
+
 }
